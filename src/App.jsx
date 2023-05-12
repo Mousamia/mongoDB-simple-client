3b3aa7064552a6a675a9d38ccc703521652ff302
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-05-12 23:53:59 +0600

    data added to DB

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,23 @@ function App() {
 
   const HandleUser = (event) =>{
     event.preventDefault();
-    console.log(event);
     const name = event.target.name.value;
     const email = event.target.email.value;
     const user = { name, email};
     console.log(user);
 
-    
-
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(error => {
+        console.log(error);
+      });
 
 
   }
